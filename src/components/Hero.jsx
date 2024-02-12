64ac02bcfa6c11a4ced910be965d17d7099d7159
EmmyTech21 <128182947+EmmyTech21@users.noreply.github.com>
2024-02-12 20:30:29 +0100

    error change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,13 +14,13 @@ const Hero = () => {
             turning ideas into interactive reality
           </p>
           <div className='flex gap-x-4 mt-4'>
-            <a href='#'>
+            <a href='https://github.com/EmmyTech21'>
               <FaGithubSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
             </a>
-            <a href='#'>
+            <a href='https://www.linkedin.com/in/adewale-emmanuelfrontendeveloper/'>
               <FaLinkedin className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
             </a>
-            <a href='#'>
+            <a href='https://twitter.com/Tech_Emmi'>
               <FaTwitterSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
             </a>
           </div>
